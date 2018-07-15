4540f783629a03926f8d4e110758cc6226ee6ef8
Mario Garcia <mario.ggar@gmail.com>
2018-07-16 00:24:28 +0200

    Fix deploy script header

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,3 +1,25 @@
 #!/usr/bin/env bash
 
-echo "DEPLOYED!"
+set -o pipefail
+set -o errexit
+set -o nounset"
+
+# DESCRIPTION
+# ===========
+#
+# This script can be used to apply a deployment to k8s using any
+# continuous integration environment.
+#
+# - K8S_CA_CRT: Host certificate
+# - K8S_CLUSTER_NAME: Cluster name
+# - K8S_HOST: Address name of the K8s cluster
+# - K8S_CONTEXT_NAME: Namespace and context name
+# - K8S_USERNAME: ServiceAccount name
+# - K8S_USER_TOKEN: ServiceAccount token
+#
+# Once the credentials have been set then we can apply the deployment,
+# pods, services...etc yaml files
+#
+# Finally the scripts deletes the server certificate
+
+echo "DEPLOYED!
